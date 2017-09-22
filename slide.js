64eafba3a45ca25350d1fd7a4ce5d6db9c763993
Dmitri Chebotarev <dmitri.chebotarev@gmail.com>
2017-09-22 23:49:00 +0200

    Added slide system

--- slide.js
+++ slide.js
@@ -1,3 +1,30 @@
-$(function () {
+(function($){
+    $(function () {
+        var wrapper = $('#wrapper');
+        var animationSpeed = 500;
 
-});
+        $(document).on('slide.show', function(event){
+            show(event.id);
+        });
+
+        function show(id){
+            slideDown();
+            $('.page').hide();
+            $('#'+id).show();
+            slideUp();
+        }
+        
+        function slideDown() {
+            wrapper.animate({
+                bottom: '-45%'
+            }, animationSpeed);
+        }
+        
+        function slideUp() {
+            wrapper.animate({
+                bottom: 0
+            }, animationSpeed);
+        }
+    
+    });
+})(jQuery);
