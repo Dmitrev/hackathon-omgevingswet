650d1f776789ff196cc3134bd28f33019a1e97cf
Daniël Pels <daniel@gobletgames.com>
2017-09-23 00:19:21 +0200

    krijgt alle kleur codes van 100,100

--- daniel.js
+++ daniel.js
@@ -11,11 +11,30 @@ var SimpleGame = /** @class */ (function () {
     }
     SimpleGame.prototype.preload = function () {
         this.game.load.image("map", "TestMap.png");
-        this.game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
-        //this.game.scale.height = window.innerHeight;
+        //this.game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
+        this.inputEnabled = true;
     };
     SimpleGame.prototype.create = function () {
-        this.map = this.game.add.sprite(0, 0, "map");
+        var _this = this;
+        this.map = this.game.add.sprite(500, 500, "map");
+        this.bitmapMap = this.game.add.bitmapData(this.map.width, this.map.height);
+        console.log(this.map.width, this.map.height);
+        this.bitmapMap.draw(this.game.cache.getImage("map"));
+        this.bitmapMap.update();
+        //this.bitmapMap.addToWorld(0, 0);
+        this.map.inputEnabled = true;
+        this.map.events.onInputDown.add(function (sprite, point) {
+            var localPosition = new Phaser.Point(point.position.x - _this.map.x, point.position.y - _this.map.y);
+            var hashmap = {};
+            for (var x = 0, len = 100; x < len; x++) {
+                for (var y = 0, len_1 = 100; y < len_1; y++) {
+                    var p = _this.bitmapMap.getPixel(Phaser.Math.clamp(x - 50, 0, 100), Phaser.Math.clamp(y - 50, 0, 100));
+                    var hexString = Phaser.Color.RGBtoString(p.r, p.g, p.b, p.a);
+                    hashmap[hexString] = true;
+                }
+            }
+            console.log(hashmap);
+        });
     };
     SimpleGame.prototype.update = function () {
     };
--- daniel.ts
+++ daniel.ts
@@ -14,15 +14,40 @@ class SimpleGame {
 
     game: Phaser.Game;
     map: Phaser.Sprite;
+    bitmapMap: Phaser.BitmapData;
+    inputEnabled: boolean;
 
     preload() {
         this.game.load.image("map", "TestMap.png");
-        this.game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
-        //this.game.scale.height = window.innerHeight;
+        //this.game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
+        this.inputEnabled = true;
     }
 
     create() {
-        this.map = this.game.add.sprite(0, 0, "map");
+        this.map = this.game.add.sprite(500, 500, "map");
+
+        this.bitmapMap = this.game.add.bitmapData(this.map.width, this.map.height);
+        console.log(this.map.width, this.map.height);
+        this.bitmapMap.draw(this.game.cache.getImage("map"));
+        this.bitmapMap.update();
+        //this.bitmapMap.addToWorld(0, 0);
+
+        this.map.inputEnabled = true;
+        this.map.events.onInputDown.add((sprite: Phaser.Sprite, point: Phaser.Pointer) => {
+            let localPosition: Phaser.Point = new Phaser.Point(point.position.x - this.map.x, point.position.y - this.map.y);
+
+            let hashmap: any = {};
+
+            for (let x = 0, len = 100; x < len; x++) {
+                for (let y = 0, len = 100; y < len; y++) {
+                    let p: any = this.bitmapMap.getPixel(Phaser.Math.clamp(x - 50, 0, 100), Phaser.Math.clamp(y - 50, 0, 100));
+                    let hexString: string = Phaser.Color.RGBtoString(p.r, p.g, p.b, p.a);
+                    hashmap[hexString] = true;
+                }
+            }
+
+            console.log(hashmap);
+        })
     }
 
     update() {
