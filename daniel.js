5f114ed832e49e4893cf4f2a2db36080a45c0d83
Daniël Pels <daniel@gobletgames.com>
2017-09-23 10:28:33 +0200

    iets gedaan

--- daniel.js
+++ daniel.js
@@ -1,6 +1,7 @@
 /// <reference path="node_modules/phaser/typescript/pixi.comments.d.ts" />
 /// <reference path="node_modules/phaser/typescript/phaser.comments.d.ts" />
 /// <reference path="node_modules/phaser/typescript/p2.d.ts" />
+/// <reference path="jquery.d.ts" />
 var SimpleGame = /** @class */ (function () {
     function SimpleGame() {
         this.game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'map', {
@@ -12,16 +13,16 @@ var SimpleGame = /** @class */ (function () {
     SimpleGame.prototype.preload = function () {
         this.game.load.image("map", "TestMap.png");
         this.game.load.image("marker", "LocationMarker.png");
-        //this.game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
         this.inputEnabled = true;
     };
     SimpleGame.prototype.create = function () {
         var _this = this;
         var radius = 400;
         this.map = this.game.add.sprite(0, 0, "map");
+        //region geruiker markers
         this.graphicc = this.game.add.graphics(-1000, -1000);
         this.graphicc.lineStyle(2, Phaser.Color.getColor32(200, 0, 97, 255), 0.6);
-        this.graphicc.beginFill(Phaser.Color.getColor32(200, 66, 138, 255), 0.8);
+        this.graphicc.beginFill(Phaser.Color.getColor32(200, 66, 138, 255), 0.5);
         this.graphicc.drawCircle(0, 0, radius / 2);
         this.graphicc.endFill();
         this.map.addChild(this.graphicc);
@@ -29,40 +30,30 @@ var SimpleGame = /** @class */ (function () {
         this.map.addChild(this.mapMarker);
         this.mapMarker.scale.set(0.4, 0.4);
         this.mapMarker.anchor.set(0.5, 1);
+        //endregion
+        //region sydny
+        this.sydnyCircle = this.game.add.graphics(1093, 379);
+        this.sydnyCircle.lineStyle(2, Phaser.Color.getColor32(200, 0, 97, 255), 0.6);
+        this.sydnyCircle.beginFill(Phaser.Color.getColor32(60, 72, 244, 66), 0.5);
+        this.sydnyCircle.drawCircle(0, 0, radius / 2);
+        this.sydnyCircle.endFill();
+        this.map.addChild(this.sydnyCircle);
+        this.sydnyMarker = this.game.add.sprite(1093, 379, "marker");
+        this.map.addChild(this.sydnyMarker);
+        this.sydnyMarker.scale.set(0.4, 0.4);
+        this.sydnyMarker.anchor.set(0.5, 1);
+        this.sydnyMarker.inputEnabled = true;
+        this.sydnyMarker.events.onInputDown.add(function () {
+            var aa = $.Event("slide.show");
+            aa.id = "main-category";
+            $(document).trigger(aa);
+        });
+        //endregion
         this.bitmapMap = this.game.add.bitmapData(this.map.width, this.map.height);
         console.log(this.map.width, this.map.height);
         this.bitmapMap.draw(this.game.cache.getImage("map"));
         this.bitmapMap.update();
-        //this.bitmapMap.addToWorld(0, 0);
         this.map.inputEnabled = true;
-        /*this.map.events.onInputDown.add((sprite: Phaser.Sprite, point: Phaser.Pointer) => {
-            let localPosition: Phaser.Point = new Phaser.Point(point.position.x - this.map.x, point.position.y - this.map.y);
-
-            this.graphicc.position.set(localPosition.x, localPosition.y);
-
-            let returnArray: any = [];
-
-
-            let hashMap: any = {};
-
-            for (let x = 0, len = radius; x < len; x++) {
-                for (let y = 0, len = radius; y < len; y++) {
-                    let p: any = this.bitmapMap.getPixel(Phaser.Math.clamp((localPosition.x + (x - radius * 0.5)), 0, this.map.width), Phaser.Math.clamp((localPosition.y + (y - radius * 0.5)), 0, this.map.height));
-                    let hexString: string = Phaser.Color.RGBtoString(p.r, p.g, p.b, p.a);
-                    hashMap[hexString] = hexString;
-                    //this.bitmapMap.setPixel(Phaser.Math.clamp((localPosition.x + (x - radius*0.5)), 0, this.map.width), Phaser.Math.clamp((localPosition.y + (y - radius*0.5)), 0, this.map.height), 255, 0, 0, false);
-                }
-            }
-            //this.bitmapMap.context.putImageData(this.bitmapMap.imageData, 0, 0);
-            //this.bitmapMap.dirty = true;
-            //this.bitmapMap.update();
-
-            for (let value in hashMap) {
-                returnArray.push(value);
-            }
-
-            console.log(returnArray);
-        });*/
         this.map.input.draggable = true;
         this.map.events.onDragStop.add(function (sprite, point) {
             console.log(point.timeUp - point.timeDown);
@@ -78,18 +69,24 @@ var SimpleGame = /** @class */ (function () {
                 for (var y = 0, len_1 = radius; y < len_1; y++) {
                     var p = _this.bitmapMap.getPixel(Phaser.Math.clamp((localPosition.x + (x - radius * 0.5)), 0, _this.map.width), Phaser.Math.clamp((localPosition.y + (y - radius * 0.5)), 0, _this.map.height));
                     var hexString = Phaser.Color.RGBtoString(p.r, p.g, p.b, p.a);
-                    hashMap[hexString] = hexString;
-                    //this.bitmapMap.setPixel(Phaser.Math.clamp((localPosition.x + (x - radius*0.5)), 0, this.map.width), Phaser.Math.clamp((localPosition.y + (y - radius*0.5)), 0, this.map.height), 255, 0, 0, false);
+                    if (hashMap.hasOwnProperty(hexString)) {
+                        hashMap[hexString]++;
+                    }
+                    else {
+                        hashMap[hexString] = 1;
+                    }
                 }
             }
-            //this.bitmapMap.context.putImageData(this.bitmapMap.imageData, 0, 0);
-            //this.bitmapMap.dirty = true;
-            //this.bitmapMap.update();
             for (var value in hashMap) {
                 returnArray.push(value);
             }
-            console.log(returnArray);
-            colors = returnArray;
+            console.log(hashMap);
+            //colors = returnArray;
+            colors = hashMap;
+        }, this);
+        var key1 = this.game.input.keyboard.addKey(Phaser.Keyboard.ONE);
+        key1.onDown.add(function () {
+            alert("nieuwe post");
         }, this);
     };
     SimpleGame.prototype.update = function () {
--- daniel.ts
+++ daniel.ts
@@ -1,6 +1,7 @@
 /// <reference path="node_modules/phaser/typescript/pixi.comments.d.ts" />
 /// <reference path="node_modules/phaser/typescript/phaser.comments.d.ts" />
 /// <reference path="node_modules/phaser/typescript/p2.d.ts" />
+/// <reference path="jquery.d.ts" />
 
 declare let colors;
 
@@ -22,10 +23,13 @@ class SimpleGame {
     graphicc: Phaser.Graphics;
     mapMarker: Phaser.Sprite;
 
+    sydnyMarker: Phaser.Sprite;
+    sydnyCircle: Phaser.Graphics;
+
+
     preload() {
         this.game.load.image("map", "TestMap.png");
         this.game.load.image("marker", "LocationMarker.png");
-        //this.game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
         this.inputEnabled = true;
     }
 
@@ -33,54 +37,49 @@ class SimpleGame {
         let radius: number = 400;
 
         this.map = this.game.add.sprite(0, 0, "map");
+
+        //region geruiker markers
         this.graphicc = this.game.add.graphics(-1000, -1000);
         this.graphicc.lineStyle(2, Phaser.Color.getColor32(200, 0, 97, 255), 0.6);
-        this.graphicc.beginFill(Phaser.Color.getColor32(200, 66, 138, 255), 0.8);
+        this.graphicc.beginFill(Phaser.Color.getColor32(200, 66, 138, 255), 0.5);
         this.graphicc.drawCircle(0, 0, radius / 2);
         this.graphicc.endFill();
         this.map.addChild(this.graphicc);
-
 
         this.mapMarker = this.game.add.sprite(-1000, -1000, "marker");
         this.map.addChild(this.mapMarker);
         this.mapMarker.scale.set(0.4, 0.4);
         this.mapMarker.anchor.set(0.5, 1);
+        //endregion
+
+        //region sydny
+        this.sydnyCircle = this.game.add.graphics(1093, 379);
+        this.sydnyCircle.lineStyle(2, Phaser.Color.getColor32(200, 0, 97, 255), 0.6);
+        this.sydnyCircle.beginFill(Phaser.Color.getColor32(60, 72, 244, 66), 0.5);
+        this.sydnyCircle.drawCircle(0, 0, radius / 2);
+        this.sydnyCircle.endFill();
+        this.map.addChild(this.sydnyCircle);
+
+        this.sydnyMarker = this.game.add.sprite(1093, 379, "marker");
+        this.map.addChild(this.sydnyMarker);
+        this.sydnyMarker.scale.set(0.4, 0.4);
+        this.sydnyMarker.anchor.set(0.5, 1);
+        this.sydnyMarker.inputEnabled = true;
+        this.sydnyMarker.events.onInputDown.add(() => {
+            let aa: any = $.Event("slide.show");
+            aa.id = "main-category";
+
+            $(document).trigger(aa);
+        });
+        //endregion
+
 
         this.bitmapMap = this.game.add.bitmapData(this.map.width, this.map.height);
         console.log(this.map.width, this.map.height);
         this.bitmapMap.draw(this.game.cache.getImage("map"));
         this.bitmapMap.update();
-        //this.bitmapMap.addToWorld(0, 0);
 
         this.map.inputEnabled = true;
-        /*this.map.events.onInputDown.add((sprite: Phaser.Sprite, point: Phaser.Pointer) => {
-            let localPosition: Phaser.Point = new Phaser.Point(point.position.x - this.map.x, point.position.y - this.map.y);
-
-            this.graphicc.position.set(localPosition.x, localPosition.y);
-
-            let returnArray: any = [];
-
-
-            let hashMap: any = {};
-
-            for (let x = 0, len = radius; x < len; x++) {
-                for (let y = 0, len = radius; y < len; y++) {
-                    let p: any = this.bitmapMap.getPixel(Phaser.Math.clamp((localPosition.x + (x - radius * 0.5)), 0, this.map.width), Phaser.Math.clamp((localPosition.y + (y - radius * 0.5)), 0, this.map.height));
-                    let hexString: string = Phaser.Color.RGBtoString(p.r, p.g, p.b, p.a);
-                    hashMap[hexString] = hexString;
-                    //this.bitmapMap.setPixel(Phaser.Math.clamp((localPosition.x + (x - radius*0.5)), 0, this.map.width), Phaser.Math.clamp((localPosition.y + (y - radius*0.5)), 0, this.map.height), 255, 0, 0, false);
-                }
-            }
-            //this.bitmapMap.context.putImageData(this.bitmapMap.imageData, 0, 0);
-            //this.bitmapMap.dirty = true;
-            //this.bitmapMap.update();
-
-            for (let value in hashMap) {
-                returnArray.push(value);
-            }
-
-            console.log(returnArray);
-        });*/
         this.map.input.draggable = true;
         this.map.events.onDragStop.add((sprite: Phaser.Sprite, point: Phaser.Pointer) => {
             console.log(point.timeUp - point.timeDown);
@@ -101,22 +100,27 @@ class SimpleGame {
                 for (let y = 0, len = radius; y < len; y++) {
                     let p: any = this.bitmapMap.getPixel(Phaser.Math.clamp((localPosition.x + (x - radius * 0.5)), 0, this.map.width), Phaser.Math.clamp((localPosition.y + (y - radius * 0.5)), 0, this.map.height));
                     let hexString: string = Phaser.Color.RGBtoString(p.r, p.g, p.b, p.a);
-                    hashMap[hexString] = hexString;
-                    //this.bitmapMap.setPixel(Phaser.Math.clamp((localPosition.x + (x - radius*0.5)), 0, this.map.width), Phaser.Math.clamp((localPosition.y + (y - radius*0.5)), 0, this.map.height), 255, 0, 0, false);
+                    if (hashMap.hasOwnProperty(hexString)) {
+                        hashMap[hexString]++
+                    } else {
+                        hashMap[hexString] = 1;
+                    }
                 }
             }
-            //this.bitmapMap.context.putImageData(this.bitmapMap.imageData, 0, 0);
-            //this.bitmapMap.dirty = true;
-            //this.bitmapMap.update();
 
             for (let value in hashMap) {
                 returnArray.push(value);
             }
 
-            console.log(returnArray);
+            console.log(hashMap);
 
+            //colors = returnArray;
+            colors = hashMap;
+        }, this);
 
-            colors = returnArray;
+        let key1 = this.game.input.keyboard.addKey(Phaser.Keyboard.ONE);
+        key1.onDown.add(() => {
+            alert("nieuwe post");
         }, this);
     }
 
