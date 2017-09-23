55d21db3206d16581456301ec0196794eb8598ee
Dmitri Chebotarev <dmitri.chebotarev@gmail.com>
2017-09-23 07:58:47 +0200

    Added card template

--- ui.js
+++ ui.js
@@ -6,6 +6,13 @@ $(function(){
       esc: 27
     };
 
+    var cardTemplate =
+        '<div class="col-sm-4">'+
+        '<div class="card" data-slide="option" data-category="{index}">' +
+        '<div class="card-body">{text}</div>' +
+        '</div>'+
+        '</div>';
+
     $(document).on('keydown', function (e) {
         console.log(e.keyCode);
 
@@ -31,9 +38,12 @@ $(function(){
     });
 
     $('[data-slide]').on('click', function () {
-        // console.log($(this));
+        // If category button is clicked
         if( typeof $(this).attr('data-category') === 'string'){
             category = $(this).attr('data-category');
+
+            console.log(categories[category]);
+
         }
         var target = $(this).data('slide');
         trigger(target);
@@ -43,6 +53,6 @@ $(function(){
         $(document).trigger('slide.hide');
     });
 
-
+A
 
 });
